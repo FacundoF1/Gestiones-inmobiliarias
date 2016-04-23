1b98ebb24bd7413a308518f0bf57012ab0580818
FacundoF1 <FacundoF1@hotmail.com.ar>
2016-04-23 10:38:37 -0300

    Update cnn.php

--- Connections/cnn.php
+++ Connections/cnn.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_cnn = "localhost";
-$database_cnn = "tesis";
-$username_cnn = "root";
-$password_cnn = "12061";
+$hostname_cnn = "";
+$database_cnn = "";
+$username_cnn = "";
+$password_cnn = "";
 $cnn = mysql_pconnect($hostname_cnn, $username_cnn, $password_cnn) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
